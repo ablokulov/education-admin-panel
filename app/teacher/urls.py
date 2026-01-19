5e4584b4902e3a169c1d8f2ad78004c421e289af
Nodirbek <ablokulovnodirbek4@gmail.com>
2026-01-19 13:04:39 +0500

    done

--- app/teacher/urls.py
+++ app/teacher/urls.py
@@ -4,5 +4,5 @@ from .views import ListCreateViews,UpdateViews
 
 urlpatterns = [
     path('teachers/',ListCreateViews.as_view(),name="teachers"),
-    path('teachers/<int:id>/',UpdateViews.as_view(),name="teachers"),
+    path('teachers/<int:id>/',UpdateViews.as_view(),name="teachers")
 ]
--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('app.users.urls')),
     path('api/',include('app.teacher.urls')),
+    path('api/',include('app.groups.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
