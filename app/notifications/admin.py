2ceb311cd4acb38aaeef80965d5220c3c0b0fabe
ablokulov <abloqulovnodirbek4@gmail.com>
2026-01-28 23:18:02 +0500

    versiya 2.0.0.

--- app/notifications/admin.py
+++ app/notifications/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "title",
+        "is_read",
+        "created_at",
+    )
+    list_filter = ("is_read", "created_at")
+    search_fields = ("title", "message", "user__username", "user__email")
+    ordering = ("-created_at",)
